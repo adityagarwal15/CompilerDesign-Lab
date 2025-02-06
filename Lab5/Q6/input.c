e13dba664034cb016e9b23a44ca674882652aed7
ADITYA AGARWAL <153291039+adityagarwal15@users.noreply.github.com>
2025-02-06 10:19:49 +0530

    Add files via upload

---
+++ Lab5/Q6/input.c
@@ -0,0 +1,10 @@
+/* This is a MULTI-LINE Comment
+   SHOULD NOT BE CONVERTED */
+#include <stdio.h>
+
+// THIS IS A SINGLE LINE COMMENT
+int MAIN() {
+    printf("THIS STRING SHOULD NOT BE CONVERTED\n");
+    int COUNT = 0;
+    return 0;
+}
---
+++ Lab5/Q6/output.c
@@ -0,0 +1,10 @@
+/* This is a MULTI-LINE Comment
+   SHOULD NOT BE CONVERTED */
+#include <stdio.h>
+
+// THIS IS A SINGLE LINE COMMENT
+int main() {
+    printf("THIS STRING SHOULD NOT BE CONVERTED\n");
+    int count = 0;
+    return 0;
+}
